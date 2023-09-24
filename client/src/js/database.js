549f801c43604d2aab97a498c18e514f645871c9
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-09-23 18:26:16 -0700

    added more logic to get ALL content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,10 +19,19 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('Data saved to the databse', result)
+  console.log('Data saved to the database', result)
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // use getAll() method
+  const request = store.getAll();
+  const result = await request;
+  console.log('Data read from database', result);
+  return result.value;
+};
 
 initdb();
